7d0cec70235ae8f76f2c873fdc46403453d259ea
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2021-09-18 00:09:16 +0530

    3 ways of using emojis in python program
    3 ways of using emojis in python program

---
+++ Emojis.py
@@ -0,0 +1,36 @@
+import emoji
+
+#Emoji Module
+print(emoji.emojize('Python is :thumbs_up:'))
+print(emoji.emojize('Python is :kissing_closed_eyes:'))
+print(emoji.emojize('Python is :laughing:'))
+print(emoji.emojize('Python is :kissing_heart:'))
+print(emoji.emojize('Python is :grinning:'))
+
+#Unicode
+print("\U0001F600") #This is smiley for grinning face
+print("\U0001F603") #This is smiley for grinning face with big eyes
+print("\U0001F604") #This is smiley for grinning face with smiling eyes
+print("\U0001F601") #This is smiley for beaming face with smiling eyes
+print("\U0001F606") #This is smiley for grinning squinting face
+print("\U0001F605") #This is smiley for grinning face with sweat
+print("\U0001F923") #This is smiley for rolling on the floor laughing
+print("\U0001F602") #This is smiley for face with tears of joy
+print("\U0001F642") #This is smiley for slightly smiling face
+print("\U0001F643") #This is smiley for upside-down face
+print("\U0001FAE0") #This is smiley for melting face
+print("\U0001F609") #This is smiley for winking face
+print("\U0001F60A") #This is smiley for smiling face with smiling eyes
+print("\U0001F607") #This is smiley for smiling face with halo
+
+#CLDR Names
+print("This is grinning face Smiley: ""\N{grinning face}")
+print("This is grinning face with smiling eyes Smiley: ""\N{grinning face with smiling eyes}")
+print("This is rolling on the floor laughing Smiley: ""\N{rolling on the floor laughing}")
+print("This is face with tears of joy Smiley: ""\N{face with tears of joy}")
+print("This is slightly smiling face Smiley: ""\N{slightly smiling face}")
+print("This is upside-down face Smiley: ""\N{upside-down face}")
+print("This is winking face Smiley: ""\N{winking face}")
+print("This is smiling face with smiling eyes Smiley: ""\N{smiling face with smiling eyes}")
+print("This is smiling face with halo Smiley: ""\N{smiling face with halo}")
+
